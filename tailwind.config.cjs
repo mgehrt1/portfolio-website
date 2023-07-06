b8d6c7319c814fb3bfbe5adf082bb23c801eb3be
Matthew Gehrt <mgehrt@hbs.net>
2023-07-06 16:00:42 -0500

    some animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,7 +14,33 @@ module.exports = {
 				'teal': '#52B5B5',
 				'light-blue': '#212C3F',
 				'techElement': 'rgba(43, 72, 82, 0.5)',
+			},
+			keyframes: {
+				slideInLeft: {
+					'0%': { transform: 'translateX(-200%)', opacity: '0' },
+					'30%': { transform: 'translateX(-200%)', opacity: '0' },
+					'100%': { transform: 'translateX(0)', opacity: '1' },
+				},
+				slideInTop: {
+					'0%': { transform: 'translateY(-200%)', opacity: '0' },
+					'30%': { transform: 'translateY(-200%)', opacity: '0' },
+					'100%': { transform: 'translateY(0)', opacity: '1' },
+				},
+				hoverScale: {
+					'0%': { transform: 'scale(1)'},
+					'100%': { transform: 'scale(1.05)'},
+				}
+			},
+			animation: {
+				'slide-in-left': 'slideInLeft 1.5s ease-out',
+				'slide-in-top': 'slideInTop 1.5s ease-out',
+				'scale': 'hoverScale 0.2s ease-in-out forwards',
 			}
+		},
+	},
+	variants: {
+		extend: {
+		  	animation: ['hover', 'focus'],
 		},
 	},
 	plugins: [],
